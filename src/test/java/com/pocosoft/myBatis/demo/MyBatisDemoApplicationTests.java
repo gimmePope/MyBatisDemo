571fb7dc8335ed3b2e511f6a0a569400dead10dd
Otulana Olujimi <jimiotulana@gmail.com>
2022-01-24 12:23:16 +0100

    Empty project creation for Mybatis Demo

---
+++ src/test/java/com/pocosoft/myBatis/demo/MyBatisDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pocosoft.myBatis.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBatisDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
